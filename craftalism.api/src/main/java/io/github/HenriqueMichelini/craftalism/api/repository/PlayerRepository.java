515168b9aa4543cf3bc60307d5e38336e65fe4c2
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-11-13 17:16:58 -0300

    feat: added PlayerRepository.java finders

--- craftalism.api/src/main/java/io/github/HenriqueMichelini/craftalism/api/repository/PlayerRepository.java
+++ craftalism.api/src/main/java/io/github/HenriqueMichelini/craftalism/api/repository/PlayerRepository.java
@@ -3,7 +3,10 @@ package io.github.HenriqueMichelini.craftalism.api.repository;
 import io.github.HenriqueMichelini.craftalism.api.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PlayerRepository extends JpaRepository<Player, UUID> {
+    Optional<Player> findByUuid(UUID uuid);
+    Optional<Player> findByName(String name);
 }
