ba8cd64af8050a96c4744ee0af309d86f50ed25f
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-11-13 17:14:12 -0300

    feat: added BalanceRepository.java

---
+++ craftalism.api/src/main/java/io/github/HenriqueMichelini/craftalism/api/repository/BalanceRepository.java
@@ -0,0 +1,13 @@
+package io.github.HenriqueMichelini.craftalism.api.repository;
+
+import io.github.HenriqueMichelini.craftalism.api.model.Balance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface BalanceRepository extends JpaRepository<Balance, UUID> {
+    Optional<Balance> findByUuid(UUID uuid);
+}
