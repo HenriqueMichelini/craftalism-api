82ec0162481aabf2249e84eb54dfb555c6b60089
HenriqueMichelini <henrique.g.michelini@gmail.com>
2025-11-14 18:02:55 -0300

    feat: added swagger support for Player classes and added DTOs for PlayerController.java

--- craftalism.api/src/main/java/io/github/HenriqueMichelini/craftalism/api/controller/PlayerController.java
+++ craftalism.api/src/main/java/io/github/HenriqueMichelini/craftalism/api/controller/PlayerController.java
@@ -1,33 +1,119 @@
 package io.github.HenriqueMichelini.craftalism.api.controller;
 
+import io.github.HenriqueMichelini.craftalism.api.dto.BalanceResponseDTO;
+import io.github.HenriqueMichelini.craftalism.api.dto.PlayerRequestDTO;
+import io.github.HenriqueMichelini.craftalism.api.dto.PlayerResponseDTO;
+import io.github.HenriqueMichelini.craftalism.api.mapper.PlayerMapper;
 import io.github.HenriqueMichelini.craftalism.api.model.Player;
 import io.github.HenriqueMichelini.craftalism.api.service.PlayerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/players")
+@Tag(name = "Player", description = "Player data management")
 public class PlayerController {
 
     private final PlayerService service;
+    private final PlayerMapper mapper;
 
-    public PlayerController(PlayerService service) {
+    public PlayerController(PlayerService service, PlayerMapper mapper) {
         this.service = service;
+        this.mapper = mapper;
     }
 
+    @Operation(
+            summary = "Get Player by UUID",
+            description = "Returns the player information for a specific UUID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Player found successfully",
+                    content = @Content(schema = @Schema(implementation = PlayerResponseDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Player not found for this UUID",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
     @GetMapping("/{uuid}")
-    public Player getPlayerByUuid(@PathVariable UUID uuid) {
-        return service.getPlayerByUuid(uuid);
+    public ResponseEntity<PlayerResponseDTO> getPlayerByUuid(
+            @Parameter(description = "Player UUID", example = "550e8400-e29b-41d4-a716-446655440000")
+            @PathVariable UUID uuid
+    ) {
+        Player player = service.getPlayerByUuid(uuid);
+
+        return ResponseEntity
+                .ok(mapper.toDto(player));
     }
 
+    @Operation(
+            summary = "Get Player by name",
+            description = "Returns the player information for a specific name"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Player found successfully",
+                    content = @Content(schema = @Schema(implementation = PlayerResponseDTO.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Player not found for this name",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
     @GetMapping("/name/{name}")
-    public Player getPlayerByName(@PathVariable String name) {
-        return service.getPlayerByName(name);
+    public ResponseEntity<PlayerResponseDTO> getPlayerByName(
+            @Parameter(description = "Player name", example = "KOLONY_9")
+            @PathVariable String name
+    ) {
+        Player player = service.getPlayerByName(name);
+
+        return ResponseEntity
+                .ok(mapper.toDto(player));
     }
 
+    @Operation(
+            summary = "Create new player",
+            description = "Creates a new player with it's name and UUID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Player created successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Player already exists for this UUID"
+            )
+    })
     @PostMapping
-    public Player createPlayer(@RequestParam UUID uuid, @RequestParam String name) {
-        return service.createPlayer(uuid, name);
+    public ResponseEntity<PlayerResponseDTO> createPlayer(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Player data",
+                    required = true
+            )
+            @RequestBody @Valid PlayerRequestDTO dto
+    ) {
+        Player created = service.createPlayer(dto.uuid(), dto.name());
+
+        return ResponseEntity
+                .created(URI.create("/api/players/" + created.getUuid()))
+                .body(mapper.toDto(created));
     }
 }
--- craftalism.api/src/main/java/io/github/HenriqueMichelini/craftalism/api/dto/PlayerResponseDTO.java
+++ craftalism.api/src/main/java/io/github/HenriqueMichelini/craftalism/api/dto/PlayerResponseDTO.java
@@ -1,6 +1,16 @@
 package io.github.HenriqueMichelini.craftalism.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
-public record PlayerResponseDTO(UUID uuid, String name) {
+public record PlayerResponseDTO(
+        @Schema(description = "Player unique identifier", example = "550e8400-e29b-41d4-a716-446655440000")
+        UUID uuid,
+
+        @Schema(description = "Player name", example = "KOLONY_9",  minLength = 3, maxLength = 16)
+        @NotBlank(message = "Name is required")
+        String name
+    ) {
 }
